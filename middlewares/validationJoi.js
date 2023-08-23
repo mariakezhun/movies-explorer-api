f8b37d5700fc516ef300a2f74a18ed4d293440fc
Maria Kezhun <marusa.kezhoon@yandex.ru>
2023-08-23 18:06:48 +0300

    fix validation

--- middlewares/validationJoi.js
+++ middlewares/validationJoi.js
@@ -7,8 +7,6 @@ const createUserValidation = celebrate({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
     name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().pattern(regLink),
   }),
 });
 
